f3c0fcf0c6156a087e7a0e19bace87ad78da82dd
LCK <2276842367@qq.com>
2023-11-22 12:33:29 +0800

    upload

---
+++ Java/f1/src/111/Hello.java
@@ -0,0 +1,14 @@
+package ´óÊý¾ÝG226_03;
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello,World!");
+		System.out.println("Error:404 NotFound!");
+	}
+
+}
